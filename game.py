dff6426016f3646d409c3fc4aca8fd3e8d619889
itsabdullah04 <abdullah770.ah@gmail.com>
2025-04-07 00:14:46 +0100

    Added the round function docstring

--- game.py
+++ game.py
@@ -16,3 +16,18 @@ class Card:
     def __str__(self):
         # If its a special card it returns its name, otherwise show colour and number
         return f"{self.colour} {self.number}" if not self.special else self.name
+
+def round(player_card: Card, computer_card: Card, scores: dict, discard_pile: list[Card]) -> tuple[dict, list[Card]]:
+    """
+    Compares the player's and computer's cards and updated the scores and discard pile accordingly.
+
+    Parameters:
+        player_card (Card): The card chosen by the player.
+        computer_card (Card): The card chosen by the computer.
+        scores (dict): Current scores of both players.
+        discard_pile (list): the pile where the card played will be added.
+
+    Returns:
+    tuple: Updated scores and discard pile.
+    """
+    
