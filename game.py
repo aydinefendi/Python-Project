72892303532c0f59b8536ddbc53e473bd270a19f
itsabdullah04 <abdullah770.ah@gmail.com>
2025-04-07 19:35:19 +0100

    fixed minor issues

--- game.py
+++ game.py
@@ -1,28 +1,29 @@
 #ID: 5670726
 
-def calculating_points(player_card: Card, computer_card: Card,
+def calculate_points(player_card: Card, computer_card: Card,
                        previous_player_card: Card = None,
                        previous_computer_card: Card = None,
-                       winner: str = " ") -> int:
+                       winner: str = "") -> int:
 
     """
-    Calculates total points for the round including bonus.
+    Calculates total points for the round including any applicable bonus.
 
-    parameters:
-        player_card: (Card): The card played by the player.
-        computer_card: (Card): The card played by the computer.
-        previous_player_card: (Card): Previous card played by the player.
-        previous_computer_card: (Card): Previous card played by the computer.
-        winner: (str): "player" or "computer" .
+    Parameters:
+        player_card (Card): The card played by the player.
+        computer_card (Card): The card played by the computer.
+        previous_player_card (Card): Previous card played by the player.
+        previous_computer_card (Card): Previous card played by the computer.
+        winner (str): Either "player" or "computer".
 
     Returns:
         int: Total points for the round.
     """
+
     total_points = player_card.number + computer_card.number
 
     # Check for player's bonus
     if winner == "player" and previous_player_card:
-        if player_card.colour == previous_player_card.colour and player_card,number = previous_player_card.number:
+        if player_card.colour == previous_player_card.colour and player_card.number == previous_player_card.number:
             total_points += 2
 
     #check for computer's bonus
